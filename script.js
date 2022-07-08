3ae5e2103cfc540c09bb97f4cfbc0965e3633620
mamasza <107420170+mamasza@users.noreply.github.com>
2022-07-08 18:00:07 +0200

    1.2

---
+++ script.js
@@ -0,0 +1,9 @@
+function wyswietldane()
+{
+    let nag1 = document.querySelector('#nag1');
+    nag1.innerHTML = 'Magdalena Cieśla';
+    let nag2 = document.querySelector('#nag2');
+    nag2.innerHTML = 'Magdalena Cieśla';
+    let akapit = document.querySelector('#akapit');
+    akapit.innerHTML = 'Magdalena Cieśla';
+}
